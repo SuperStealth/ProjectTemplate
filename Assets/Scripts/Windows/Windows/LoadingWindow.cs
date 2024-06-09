c04eb6c130d4c832c1b735da4daaf46bed4b39fe
SolarBlaze <veselovrv64@gmail.com>
2024-06-10 00:55:46 +0300

    [UPDATE] script folders arrange

---
+++ Assets/Scripts/Windows/Windows/LoadingWindow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Game.Views
+{
+    public class LoadingWindow : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnSceneLoaded()
+        {
+
+        }
+    }
+}
